fe4ca3297e5f66a1b962454d0b41bd3b04fae314
Praveen Dias <praveendias1180@gmail.com>
2022-02-27 18:01:02 +0530

    Example from the orig repository

---
+++ example.js
@@ -0,0 +1,23 @@
+const { IgApiClient } = require('instagram-private-api');
+require('dotenv').config()
+
+const ig = new IgApiClient();
+// You must generate device id's before login.
+// Id's generated based on seed
+// So if you pass the same value as first argument - the same id's are generated every time
+ig.state.generateDevice(process.env.IG_USERNAME);
+// Optionally you can setup proxy url
+ig.state.proxyUrl = process.env.IG_PROXY;
+(async () => {
+  // Execute all requests prior to authorization in the real Android application
+  // Not required but recommended
+  await ig.simulate.preLoginFlow();
+  const loggedInUser = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+  // The same as preLoginFlow()
+  // Optionally wrap it to process.nextTick so we dont need to wait ending of this bunch of requests
+  process.nextTick(async () => await ig.simulate.postLoginFlow());
+  // Create UserFeed instance to get loggedInUser's posts
+  const userFeed = ig.feed.user(loggedInUser.pk);
+  const myPostsFirstPage = await userFeed.items();
+  console.log(myPostsFirstPage);
+})();
